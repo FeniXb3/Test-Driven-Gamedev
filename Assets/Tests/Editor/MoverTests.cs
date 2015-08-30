7bb38e4edfec65f8851cde23f5f243498a136241
FeniXb3 <fenix.b3@gmail.com>
2015-08-30 16:37:49 +0200

    First basic test to check moving left added

---
+++ Assets/Tests/Editor/MoverTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+using NSubstitute;
+
+[TestFixture]
+public class MoverTests
+{
+	[Test]
+	public void MoveLeftTest()
+	{
+		var startingPositionY = 0.0f;
+		var mover = GetMockMover(new Vector3(0.0f, startingPositionY, 0.0f));
+
+		var newPositionY = mover.MoveHorizontaly(-1.0f)
+
+		Assert.Less(newPositionY, startingPositionY);
+	}
+
+	internal IMover GetMockMover(Vector3 startingPosition)
+	{
+		var mover = Substitute.For<Mover>();
+		mover.StartingPosition = startingPosition;
+
+		return mover;
+	}
+}
