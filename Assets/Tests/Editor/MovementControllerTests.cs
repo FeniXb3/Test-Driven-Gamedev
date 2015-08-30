ae4f7eccb14317e3160c9454c985d2fba72979f1
FeniXb3 <fenix.b3@gmail.com>
2015-08-30 16:53:02 +0200

    Renamed Mover to MovementController in test

---
+++ Assets/Tests/Editor/MovementControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+using NSubstitute;
+
+[TestFixture]
+public class MovementControllerTests
+{
+	[Test]
+	public void MoveLeftTest()
+	{
+		// Arrange
+		var startingPositionY = 0.0f;
+		var movementController = GetMockMovementController(new Vector3(0.0f, startingPositionY, 0.0f));
+
+		// Act
+		var newPositionY = movementController.MoveHorizontaly(-1.0f)
+		
+		// Assert
+		Assert.Less(newPositionY, startingPositionY);
+	}
+
+	internal IMovementController GetMockMovementController(Vector3 startingPosition)
+	{
+		var movementController = Substitute.For<MovementController>();
+		movementController.StartingPosition = startingPosition;
+
+		return moveController;
+	}
+}
