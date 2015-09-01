383b59a0030000704cbca7a09c91bb2dc93d603f
FeniXb3 <fenix.b3@gmail.com>
2015-09-01 22:19:26 +0200

    Added component using MovementController and created a player with this component. That's a moment showing that the controller and tests have the same error - player is moving on different axis than it should

---
+++ Assets/Scripts/MovementComponent.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementComponent : MonoBehaviour
+{
+
+	IMovementController movementController;
+
+	void Start()
+	{
+		movementController = new MovementController(this.transform);
+	
+	}
+	
+	void Update()
+	{
+		if (Input.GetButton("Horizontal"))
+		{
+			movementController.MoveHorizontaly(Input.GetAxis("Horizontal"));
+		}
+	}
+}
