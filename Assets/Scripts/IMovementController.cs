f7b22dcf7fc41c78f35b8b42cc3a6cc95a7fcb79
FeniXb3 <fenix.b3@gmail.com>
2015-09-04 07:33:16 +0200

    Modified movement controller so that it is possible to set starting position and check what is current position.

--- Assets/Scripts/IMovementController.cs
+++ Assets/Scripts/IMovementController.cs
@@ -1,4 +1,6 @@
 ﻿public interface IMovementController
 {
+	UnityEngine.Vector3 Position { get; }
+
 	UnityEngine.Vector3 MoveHorizontaly(float value);
 }
--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -1,10 +1,24 @@
 ﻿public class MovementController : IMovementController
 {
+	public UnityEngine.Vector3 Position
+	{
+		get
+		{
+			return transform.position;
+		}
+	}
+
 	private UnityEngine.Transform transform;
 
 	public MovementController(UnityEngine.Transform transform)
 	{
 		this.transform = transform;
+	}
+
+	public MovementController(UnityEngine.Transform transform, UnityEngine.Vector3 startingPosition)
+	{
+		this.transform = transform;
+		this.transform.Translate(startingPosition);
 	}
 
 	public UnityEngine.Vector3 MoveHorizontaly(float value)
