55de62b78affb2ecb411d324d3c872aec753b2ec
FeniXb3 <fenix.b3@gmail.com>
2015-09-08 02:23:11 +0200

    Added silly implementation for getting forced value for axis without checking the name of the axis. Next test will force implementation change.

--- Assets/Scripts/InputHelper.cs
+++ Assets/Scripts/InputHelper.cs
@@ -3,6 +3,8 @@
 public class InputHelper
 {
     private static string buttonToForce;
+    private static string axisToForce;
+    private static float axisValueToForce;
 
     public static void ForceButton(string buttonName)
     {
@@ -12,5 +14,16 @@ public class InputHelper
     public static bool GetButton(string buttonName)
     {
         return buttonToForce == buttonName;
+    }
+
+    public static void ForceAxis(string axis, float value)
+    {
+        axisToForce = axis;
+        axisValueToForce = value;
+    }
+
+    public static float GetAxis(string axis)
+    {
+        return axisValueToForce;
     }
 }
