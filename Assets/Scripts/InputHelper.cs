feeef8fcc4a5b7218b730693a48b50b25b18e72a
FeniXb3 <fenix.b3@gmail.com>
2015-09-08 02:07:25 +0200

    Implemented basic methods for handling forced input, without  considering the real input yet.

---
+++ Assets/Scripts/InputHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class InputHelper
+{
+    private static string buttonToForce;
+
+    public static void ForceButton(string buttonName)
+    {
+        buttonToForce = buttonName;
+    }
+
+    public static bool GetButton(string buttonName)
+    {
+        return buttonToForce == buttonName;
+    }
+}
