c6eab5f8a0d4a178f51de481c398580014ba1570
FeniXb3 <fenix.b3@gmail.com>
2015-09-08 02:00:26 +0200

    A "proxy" class for handling user input is needed, so I added a basic test for InputHelper class that is about to handle real user input and the forced one, needed in integration tests.

---
+++ Assets/Tests/Editor/InputHelperTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+[TestFixture]
+public class InputHelperTests
+{
+    [Test]
+    public void ForceInputButtonTest()
+    {
+        var buttonToForce = "Horizontal";
+
+        InputHelper.ForceButton(buttonToForce);
+
+        Assert.AreEqual(true, InputHelper.GetButton(buttonToForce));
+    }
+}
